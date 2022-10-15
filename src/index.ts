0b6535dd4ad153be40d756000c110d862c553da6
Núria Ramoneda <nuriarai@gmail.com>
2022-10-15 16:19:48 +0200

    Add App component

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import type {
   PokemonsStructure,
   PokemonStructure,
 } from "./apiServices/types.js";
+import App from "./components/App/App.js";
 
 const pokemons: PokemonsStructure = await getPokemonList(10, 5);
-console.log(pokemons.results);
+
+const parentElement: HTMLElement = document.querySelector(".root");
+const app = new App(parentElement);
+app.render();
